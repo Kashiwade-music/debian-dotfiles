2d9d579cf1592038c57dd8e516b31b1979c03f70
kashiwade <music5d7_1415@outlook.com>
2020-07-03 13:43:25 +0900

    fix: vscodeのインストールにsnapを使うようにした

--- installprogram/installprogram.sh
+++ installprogram/installprogram.sh
@@ -20,17 +20,12 @@ wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key 
 sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main'
 sudo apt update && sudo apt install vivaldi-stable
 
-#VScode
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install code
-
 #snap
 sudo apt update
 sudo apt install snapd
+
+#VScode
+sudo snap install --classic code
 
 #Postman
 sudo snap install Postman
