881085b1608dc237183eea92df9665ff902f5954
kashiwade <music5d7_1415@outlook.com>
2020-07-03 17:30:23 +0900

    fix: snapでインストールの時vscodeはcode_code.desktop

--- changesetting/changesetting.sh
+++ changesetting/changesetting.sh
@@ -11,7 +11,7 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "gnome-screenshot --area"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Shift><Super>s"
 
-gsettings set org.gnome.shell favorite-apps "['code.desktop', 'vivaldi-stable.desktop', 'org.gnome.Terminal.desktop', 'gnome-control-center.desktop', 'org.gnome.Nautilus.desktop', 'gnome-system-monitor.desktop', 'org.gnome.tweaks.desktop', 'syncthing-ui.desktop']"
+gsettings set org.gnome.shell favorite-apps "['code_code.desktop', 'vivaldi-stable.desktop', 'org.gnome.Terminal.desktop', 'gnome-control-center.desktop', 'org.gnome.Nautilus.desktop', 'gnome-system-monitor.desktop', 'org.gnome.tweaks.desktop', 'syncthing-ui.desktop']"
 
 cp ./changesetting/forcopy/.imwheelrc ~/
 cp ./changesetting/forcopy/imwheel.desktop ~/.config/autostart/
