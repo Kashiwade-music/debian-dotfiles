111c6ded854a3def57ba5cd76e411b73ff9024c6
kashiwade <music5d7_1415@outlook.com>
2022-06-11 19:34:37 +0900

    update vscode

--- do2.sh
+++ do2.sh
@@ -26,7 +26,8 @@ echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=$(dpkg --print
 sudo apt update && sudo apt -y install vivaldi-stable
 
 
-# VSCode
+# VSCode / checled at 20220611
+sudo apt-get install wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
