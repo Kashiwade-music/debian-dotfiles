69e6f53762b28c8af33a5b1a57f005c2f54565fc
kashiwade <music5d7_1415@outlook.com>
2022-06-11 19:37:22 +0900

    update syncthing

--- do2.sh
+++ do2.sh
@@ -39,8 +39,8 @@ sudo apt install -y code
 # Postman / checled at 20220611
 sudo snap install -y postman
 
-# Syncthing
-sudo curl -s -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+# Syncthing / checled at 20220611
+sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
 sudo apt update
 sudo apt install -y syncthing
