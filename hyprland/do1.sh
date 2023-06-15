6e62d39ebbf58b817abb3e6367192e8c5a17ecc0
kashiwade <music5d7_9265@outlook.jp>
2023-06-15 22:47:13 +0900

    20230615_

--- hyprland/do1.sh
+++ hyprland/do1.sh
@@ -256,8 +256,8 @@ cd $CWDIR/build
 git clone https://github.com/Alexays/Waybar.git
 cd Waybar
 sed -i -e 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
-# meson --prefix=/usr --buildtype=plain --auto-features=enabled --wrap-mode=nodownload build
-meson --prefix=/usr --buildtype=plain --auto-features=enabled build
+meson --prefix=/usr --buildtype=plain --auto-features=enabled --wrap-mode=nodownload build
+# meson --prefix=/usr --buildtype=plain --auto-features=enabled build
 meson configure -Dexperimental=true build
 sudo ninja -C build install
 cd $CWDIR/build
